4fe59b7ecba37b483c055b7acb5241e24078798d
rojochen <jjc.roger@gmail.com>
2017-10-12 16:32:53 +0800

    rename to client

--- src/main/java/rojo/lab/feign/UserClient.java
+++ src/main/java/rojo/lab/feign/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
+public class UserClient {
 
     @Autowired
     UserFeignService userFeignService;
